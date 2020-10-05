da9e6c97f20e6d089f303c32a3a5960801700fad
Estephan Melella <estephanmelella@gmail.com>
2020-10-05 16:26:29 -0500

    lvl 1 background

--- level1.js
+++ level1.js
@@ -4,7 +4,7 @@ class LevelOne extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('lvl1background', 'assets/lvl1background.png');
+    this.load.image('lvl1background_bigger', 'assets/lvl1background_bigger.png');
     this.load.image('boss', 'assets/boss.png')
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
@@ -20,7 +20,7 @@ class LevelOne extends Phaser.Scene {
 
   create() {
     //  A simple background for our game
-    this.add.image(400, 300, 'lvl1background');
+    this.add.image(400, 300, 'lvl1background_bigger');
 
     //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = this.physics.add.staticGroup();
