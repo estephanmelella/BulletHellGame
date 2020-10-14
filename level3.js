c15d0665ba449ca0be7af6bbd06b456c7708b28f
c-briscoe <55815783+c-briscoe@users.noreply.github.com>
2020-10-14 13:34:30 -0500

    Added win star to level 3
    For some reason, the win asset wasn't loaded properly in level 3, so I changed it so that it was.

--- level3.js
+++ level3.js
@@ -7,8 +7,9 @@ class LevelThree extends Phaser.Scene {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('boss', 'assets/boss.png')
     this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
+    this.load.image('star', 'assets/explosion.png');
     this.load.image('bomb', 'assets/bomb.png');
+    this.load.image('win', 'assets/star.png')
     this.load.spritesheet('dude', 'assets/main.png', { frameWidth: 56, frameHeight: 45 });
     this.load.audio('jump', ['assets/Jump.ogg', 'assets/Jump.mp3', 'assets/Jump.m4a']);
     this.load.audio('shot', ['assets/Shot.ogg', 'assets/Shot.mp3', 'assets/Shot.m4a']);
